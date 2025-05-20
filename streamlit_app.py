04741ff3fb8662d9a15ddae0461b6fbec0239f0a
raihanablew <raihangcr28@gmail.com>
2025-05-20 08:26:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 Muhammad Raihan ablew XF")
-st.write(Muhammad Raihan ablew XF
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.write(
+    "Muhammad Raihan ablew XF."
 )
